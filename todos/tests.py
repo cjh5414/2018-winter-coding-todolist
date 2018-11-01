84351bcafaf20d9e66b789de10349aef84515c13
cjh5414 <cjh5414@gmail.com>
2018-11-02 01:08:41 +0900

    Create todo with Todo model

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from todos.models import *
+
+
+@pytest.mark.django_db
+def test_add_todo(client):
+    response = client.post('/todos/new/', {
+        'title': '알고리즘 공부',
+        'content': '알고리즘 문제 한 개 풀기'
+    })
+
+    assert Todo.objects.all().count() == 1
+    todo = Todo.objects.get(id=1)
+
+    assert todo.title == '알고리즘 공부'
+    assert todo.content == '알고리즘 문제 한 개 풀기'
+
+    assert response.json()['result'] == 'True'
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from todos.models import *
+
+
+def create(request):
+    Todo.objects.create(title=request.POST['title'], content=request.POST['content'])
+    return JsonResponse({'result': 'True'})
+
